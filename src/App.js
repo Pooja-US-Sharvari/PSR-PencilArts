b12d58cfb2dddebf8bba95e2a4c4df29efb37ffd
pooja <poojaussharvari2000@gmail.com>
2024-06-26 17:57:17 +0530

    git config --global user.email "bob@example.com"initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
@@ -18,6 +19,7 @@ function App() {
       <Contact />
       <Footer />
     </div>
+    
   );
 }
 
